0587b392f720b4af1ce54ba07dca87b7d0a7bfeb
Cesar Cavalari <cesar.cavalari@softplan.com.br>
2017-08-10 15:45:03 -0300

    Exercício 7.16 Inserindo os livros no banco de dados

--- src/br/com/caelum/camel/TestePooling.java
+++ src/br/com/caelum/camel/TestePooling.java
@@ -9,13 +9,27 @@ import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.util.jndi.JndiContext;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
 
 import br.com.caelum.livraria.modelo.Livro;
 
 public class TestePooling {
 	
 	public static void main(String[] args) throws Exception {
-		DefaultCamelContext ctx = new DefaultCamelContext();
+		MysqlConnectionPoolDataSource mysqlDs = new MysqlConnectionPoolDataSource();
+		mysqlDs.setDatabaseName("fj36_camel");
+		mysqlDs.setServerName("localhost");
+		mysqlDs.setPort(3306);
+		mysqlDs.setUser("root");
+		mysqlDs.setPassword("root");
+		
+		JndiContext jndi = new JndiContext();
+		jndi.rebind("mysqlDataSource", mysqlDs);
+		
+		DefaultCamelContext ctx = new DefaultCamelContext(jndi);
+		
 		ctx.addRoutes(new RouteBuilder() {
 			@Override
 			public void configure() throws Exception {
@@ -33,7 +47,22 @@ public class TestePooling {
 					}
 				})
 				.log("${body}")
-				.to("mock:livros");
+//				.to("mock:livros");
+				.to("direct:livros");
+				
+				from("direct:livros")
+				.split(body())
+				.process(new Processor() {
+					@Override
+					public void process(Exchange exchange) throws Exception {
+						Message inbound = exchange.getIn();
+						Livro livro = (Livro)inbound.getBody();
+						String nomeAutor = livro.getNomeAutor();
+						inbound.setHeader("nomeAutor", nomeAutor);
+					}
+				})
+				.setBody(simple("insert into Livros (nomeAutor) values (':?nomeAutor')"))
+				.to("jdbc:mysqlDataSource?useHeadersAsParameters=true");
 			}			
 		});
 		ctx.start();
